ee0c48a413ca845352f49e90d2cef02656398d7e
Peter <56368462+pstrauli@users.noreply.github.com>
2020-02-29 19:35:35 +0100

    Book Component created and added to Library

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import { render } from 'react-dom'
 
+const Book = ({title, author, pages}) => {
+  return (
+    <section>
+      <h2>{title}</h2>
+      <p>by: {author}</p>
+      <p>Pages: {pages} pages</p>
+    </section>
+  )
+}
+
 const Library = () => {
   return (
     <div>
-      Welcome to the library
+      <Book title="1984" author="George Orwell" pages={184}/>
+      <Book title="The Denial Of Death" author="Ernest Becker" pages={266}/>
+      <Book title="Lowborn" author="Kerry Hudson" pages={187}/>
     </div>
   )
 }
