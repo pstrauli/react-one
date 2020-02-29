cbecec1c208100a5b1c87baa36f12a521158fb46
Peter <56368462+pstrauli@users.noreply.github.com>
2020-02-29 18:08:30 +0100

    Created class Message

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-var style = {
-  backgroundColor: 'orange',
-  color: 'white',
-  fontFamily: 'Arial'
+class Message extends React.Component {
+  
 }
 
 
-
 ReactDOM.render(
-  title,
+  <div style={style}>
+    <h1 id="heading-element">Hello World!!!</h1>
+    <p>We're glad you're here!</p>
+  </div>,
   document.getElementById('root')
 )
