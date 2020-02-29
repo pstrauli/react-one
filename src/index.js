004096bdcf0c4b73b7bf038f0d7981dd2cd97ee3
Peter <56368462+pstrauli@users.noreply.github.com>
2020-02-29 18:22:41 +0100

    Message class deleted, skiData variable created

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-class Message extends React.Component {
-  render() {
-    console.log(this.props)
-    return (
-      <div>
-        <h1 style={{color: this.props.color}}>
-          {this.props.msg}
-        </h1>
-        <p>I'll check back in {this.props.minutes} minutes</p>
-      </div>
-    )
-  }
+let skiData = {
+  total: 50,
+  powder: 20,
+  backcountry: 10,
+  goal: 100
 }
+
+
 
 
 ReactDOM.render(<Message color="blue" msg="How are you?" minutes={3}/>, document.getElementById('root'))
