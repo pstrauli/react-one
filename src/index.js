a21f114ef2cfa31e6fa596ebae8a185cdb11310e
Peter <56368462+pstrauli@users.noreply.github.com>
2020-02-29 19:07:18 +0100

    Started afresh...created const Library

--- src/index.js
+++ src/index.js
@@ -1,42 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-let skiData = {
-  total: 50,
-  powder: 20,
-  backcountry: 10,
-  goal: 100
-}
-
-const getPercent = decimal => {
-  return decimal * 100 + '%'
-}
-
-const calcGoalProgress = (total, goal) => {
-  return getPercent(total/goal)
-}
-
-const SkiDayCounter = ({total, powder, backcountry, goal}) => {
+const Library = () => {
   return (
-    <section>
-        <div>
-          <p>Total Days: {total}</p>
-        </div>
-        <div>
-          <p>Powder Days: {powder}</p>
-        </div>
-        <div>
-          <p>Backcountry Days: {backcountry}</p>
-        </div>
-        <div>
-          <p>Goal Progress: {calcGoalProgress(total, goal)}</p>
-        </div>
-    </section>
+    <div>
+      Welcome to the library
+    </div>
   )
 }
 
-render(<SkiDayCounter 
-  total={skiData.total}
-  powder={skiData.powder}
-  backcountry={skiData.backcountry}
-  goal={skiData.goal}/>, document.getElementById('root'))
+render(
+  <Library />,
+  document.getElementById('root'))
