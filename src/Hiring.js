aa9de92c9cc9f99773b962e8ad73bd99b93d1fba
Peter <56368462+pstrauli@users.noreply.github.com>
2020-03-01 15:46:31 +0100

    App extended and modularized

---
+++ src/Hiring.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export const Hiring = () => 
+<div>
+  <p>The library is hiring. Go to our <a href="www.library.com/jobs">jobs page</a> for more info.</p>
+</div>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
+import Library from './Library'
+
 
 let bookList = [
   {"title": "Lowborn", "author": "Kerry Hudson", "pages": 187},
@@ -10,35 +12,11 @@ let bookList = [
 ]
 // array of data
 
-const Book = ({title, author, pages}) => {
-  return (
-    <section>
-      <h2>{title}</h2>
-      <p>by: {author}</p>
-      <p>Pages: {pages} pages</p>
-    </section>
-  )
-}
+
 // variable called Book with 3 parameters, returns HTML populated by those parameters
 
-const Library = ({books}) => {
-  return (
-    <div>
-      {books.map(
-        (book, i) => <Book 
-                        key={i}
-                        title={book.title} 
-                        author={book.author} 
-                        pages={book.pages}/>
-      )}
-    </div>
-  )
-}
-// variable called Library
-// takes argument of books which in render method is equated with the variable booKlist with 
-// all the info in it.
-// returns a div containing a new array created by .map() which takes books (aka bookList) and 
-// creates for each iteration an HTML tag 
+
+
 
 render(
   <Library books={bookList} />,
